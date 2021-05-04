42a5678632242b09e35b79df55f6382f23b38101
ridipblog <memorytemp5@gmail.com>
2021-05-04 07:42:32 -0700

    second comment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const port=process.env.PORT || 3000;
 app.get("/",(req,res,next)=>{
-	res.send("welcome To My New Page");
+	res.send("welcome To My New Pages");
 });
 app.listen(port,()=>{
 	console.log("Server runing");
